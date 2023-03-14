1feae72cf6ab722ff2155d877948388a91dcb367
Rivindu Fernando <fernandorivindu2@gmail.com>
2023-03-14 11:59:02 +0530

    testing commit

--- back-end/src/repository/innovation.js
+++ back-end/src/repository/innovation.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const insertInnovationDetails = async (details) => {
   try {
-    return 'hellow world';
+    return 'hello world';
   } catch (error) {
     return error;
   }
