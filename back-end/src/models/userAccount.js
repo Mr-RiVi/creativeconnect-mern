a83348dbdf234c7684824022634f1ee6048f75f5
Sanjula De Silva <86179901+SLxMAJOR@users.noreply.github.com>
2023-03-22 17:14:15 +0530

    models created

---
+++ back-end/src/models/userAccount.js
@@ -0,0 +1,42 @@
+import mongoose from 'mongoose';
+
+const user_accountSchema = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    emailAdress: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    country: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      required: true,
+      unique: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+  }
+);
+const UserAccount = mongoose.model('userAccount', user_accountSchema);
+export default UserAccount;
