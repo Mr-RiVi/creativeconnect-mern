4b7d7f0984a2ffe3b0b638283425578d5aae63a1
Sanjula De Silva <86179901+SLxMAJOR@users.noreply.github.com>
2023-04-08 14:51:31 +0530

    Started meeting crud

---
+++ back-end/src/models/meeting.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const meetingSchema = mongoose.Schema(
+  {
+    meetingId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    discription: {
+      type: String,
+      required: true,
+      unique: false,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: Number,
+      required: false,
+    },
+  },
+  {
+    versionkey: false,
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
+  }
+);
+const MeetingTable = mongoose.model('meeting', meetingSchema);
+export default MeetingTable;
