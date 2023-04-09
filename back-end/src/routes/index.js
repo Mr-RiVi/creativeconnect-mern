5b62bbda527514421c18f8d038581561317f9d61
Sanjula De Silva <86179901+SLxMAJOR@users.noreply.github.com>
2023-04-09 17:06:13 +0530

    crud completed
    Meeting little errors

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import userAccount_router from './userAccount.js';
 import innovation_router from './innovation.js';
+import meeting_router from './meeting.js';
 
 const router = express.Router();
 
 router.use('/innovation', innovation_router);
 router.use('/userAccount', userAccount_router);
+router.use('/meeting', meeting_router);
 
 export default router;
--- back-end/src/routes/meeting.js
+++ back-end/src/routes/meeting.js
@@ -1,7 +1,18 @@
 import express from 'express';
-import { meetingRegister } from '../controllers/meeting';
+import {
+  deleteMeetings,
+  getMeeting,
+  getMeetings,
+  meetingRegister,
+  updatemeeting,
+} from '../controllers/meeting';
 
 const meeting_router = express.Router();
 
 meeting_router.post('/addMeeting', meetingRegister);
-meeting_router.get('/');
+meeting_router.get('/viewAllMeetings', getMeetings);
+meeting_router.get('/viewMeeting/:Meeting_Id', getMeeting);
+meeting_router.put('/updateMeeting/:Meeting_Id', updatemeeting);
+meeting_router.delete('/deleteMeeting/:Meeting_Id', deleteMeetings);
+
+export default meeting_router;
