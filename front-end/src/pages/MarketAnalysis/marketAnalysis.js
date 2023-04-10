4208ccaa163450dfee820d112c75e5e79447b175
Rivindu Fernando <fernandorivindu2@gmail.com>
2023-04-10 19:10:37 +0530

    innovation details adding pages creating

---
+++ front-end/src/pages/MarketAnalysis/marketAnalysis.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const MarketAnalysis = () => {
+  return (
+    <div>
+      <h1>hello this is market analysis page</h1>
+    </div>
+  );
+};
+
+export default MarketAnalysis;
